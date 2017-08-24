a5f3598abde8f9b7e6e2616109692192d2c46dd9
Rion Williams <rionmonster@gmail.com>
2017-08-23 23:17:49 -0500

    Initial Commit :tada:
    Created a very early proof of concept query builder that simply handled
    a few common use cases.

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace QueryBuilder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var queryBuilder = new QueryBuilder())
+            {
+                var basic = queryBuilder.Build($@"
+                    SELECT  *
+                    FROM    YourTable
+                    WHERE   TheAnswer = { 42 }
+                ");
+
+                var collection = queryBuilder.Build($@"
+                    SELECT  *
+                    FROM    YourTable
+                    WHERE   TheAnswer IN ({ new int[] { 1, 2, 3 } })
+                ");
+
+                var reuse = queryBuilder.Build($@"
+                    SELECT  *
+                    FROM    YourTable
+                    WHERE   TheAnswer IN ({ new int[] { 1, 2, 3 } })
+                    AND     Foo = { 2 }
+                ");
+            }
+        }
+    }
+}
---
+++ src/QueryBuilder.cs
@@ -0,0 +1,94 @@
+﻿using Dapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace QueryBuilder
+{
+    public class QueryBuilder : IDisposable
+    {
+        // Backing query stores
+        private DynamicParameters _params;
+        private int _paramCount = 0;
+        private Dictionary<object, string> _paramMap = new Dictionary<object, string>();
+
+        public QueryBuilder()
+        {
+            _params = new DynamicParameters();
+        }
+
+        public string Build(FormattableString sql)
+        {
+            // Get all interpolated values and parameterize them
+            var pArgs = new List<string>();
+            foreach (var argument in sql.GetArguments())
+            {
+                switch (argument)
+                {
+                    case IList arguments:
+                        pArgs.Add(Parameterize(arguments));
+                        break;
+                    default:
+                        pArgs.Add(Parameterize(argument));
+                        break;
+                }
+            }
+
+            // Automatically handle parameterizing interpolated strings
+            return string.Format(sql.Format, pArgs.ToArray());
+        }
+
+        private string Parameterize(object parameter)
+        {
+            if (_paramMap.ContainsKey(parameter))
+            {
+                // If it exists, just use the same parameter
+                return _paramMap[parameter];
+            }
+
+            var parameterName = $"@P{++_paramCount}";
+            _params.Add(parameterName, parameter);
+            _paramMap.Add(parameter, parameterName);
+
+            return parameterName;
+        }
+
+        private string Parameterize(IList parameters)
+        {
+            var paramNames = new List<string>();
+            foreach (var parameter in parameters)
+            {
+                paramNames.Add(Parameterize(parameter));
+            }
+
+            return string.Join(",", paramNames);
+        }
+
+        #region IDisposable Support
+
+        private bool _disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _params = null;
+                    _paramCount = 0;
+                    _paramMap = null;
+                }
+
+                _disposed = true;
+            }
+        }
+
+        #endregion
+    }
+}
